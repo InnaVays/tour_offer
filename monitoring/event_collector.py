a61f8f6da02104b0843d45d33e5da3e366140c63
Inna Vays <129948497+InnaVays@users.noreply.github.com>
2021-02-12 14:20:12 +0100

    Update event_collector

--- monitoring/event_collector.py
+++ monitoring/event_collector.py
@@ -24,6 +24,28 @@ def setup_database():
     conn.commit()
     conn.close()
 
+@app.route('/api/collect-event', methods=['POST'])
+def collect_event():
+    """
+        API to collect user interactions.
+    """
+    data = request.json
+    user_id = data.get("user_id")
+    session_id = data.get("session_id")
+    event_type = data.get("event_type")
+    timestamp = data.get("timestamp", time.strftime("%Y-%m-%d %H:%M:%S"))
+
+    if not user_id or not session_id or not event_type:
+        return jsonify({"error": "Missing required fields"}), 400
+
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO user_events VALUES (?, ?, ?, ?)", 
+                   (user_id, session_id, event_type, timestamp))
+    conn.commit()
+    conn.close()
+
+    return jsonify({"message": "Event logged successfully"}), 200
 
 if __name__ == '__main__':
     setup_database()
