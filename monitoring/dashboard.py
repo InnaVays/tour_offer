059df911dd4346ad71154d9e111f33998b381075
Inna Vays <129948497+InnaVays@users.noreply.github.com>
2021-02-14 18:46:00 +0100

    Update dashboard

--- monitoring/dashboard.py
+++ monitoring/dashboard.py
@@ -13,23 +13,29 @@ def load_data():
     conn.close()
     return df
 
+# Load data
 df = load_data()
 
+# Sidebar Filters
 st.sidebar.title("Experiment Filters")
 selected_strategy = st.sidebar.selectbox("Filter by Strategy", ["All"] + df["strategy"].unique().tolist())
 
 if selected_strategy != "All":
     df = df[df["strategy"] == selected_strategy]
 
+# Calculate Metrics
 strategy_summary = df.groupby("strategy")["event_type"].value_counts().unstack(fill_value=0)
 
+# Clicks on Offer per Strategy
 clicks_df = strategy_summary.get("click", pd.Series(dtype=int)).reset_index()
 clicks_df.columns = ["Strategy", "Clicks"]
 
+# Interest per Strategy (email OR request a call)
 df["interest"] = df["event_type"].apply(lambda x: 1 if x in ["email", "request_call"] else 0)
 interest_df = df.groupby("strategy")["interest"].sum().reset_index()
 interest_df.columns = ["Strategy", "Interest"]
 
+# Display Metrics
 st.title("Travel Offer Experiment Dashboard")
 
 st.subheader("Clicks on Offer per Strategy")
