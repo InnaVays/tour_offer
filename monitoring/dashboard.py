ba53620bb03197cc11f855cfa08a7b57c4d7769e
Inna Vays <129948497+InnaVays@users.noreply.github.com>
2021-02-14 16:26:10 +0100

    Update dashboard

--- monitoring/dashboard.py
+++ monitoring/dashboard.py
@@ -22,25 +22,25 @@ ctr = round(df[df["event_type"] == "click"].shape[0] / total_events * 100, 2) if
 conversion_rate = round(df[df["event_type"] == "book"].shape[0] / total_events * 100, 2) if total_events > 0 else 0
 
 # Sidebar Filters
-st.sidebar.title("🔍 Experiment Filters")
+st.sidebar.title("Experiment Filters")
 selected_event = st.sidebar.selectbox("Filter by Event Type", ["All"] + df["event_type"].unique().tolist())
 
 if selected_event != "All":
     df = df[df["event_type"] == selected_event]
 
 # Display Metrics
-st.title("📊 Travel Offer Experiment Dashboard")
+st.title("Travel Offer Experiment Dashboard")
 st.metric("Total Events Logged", total_events)
 st.metric("Click-Through Rate (CTR)", f"{ctr}%")
 st.metric("Booking Rate", f"{conversion_rate}%")
 
 # Event Distribution Chart
-st.subheader("📈 Event Distribution")
+st.subheader("Event Distribution")
 fig_event = px.histogram(df, x="event_type", title="User Actions Distribution")
 st.plotly_chart(fig_event)
 
 # Display Data Table
-st.subheader("📋 User Event Logs")
+st.subheader("User Event Logs")
 st.dataframe(df)
 
 # Refresh every 30 seconds
