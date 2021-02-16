11bd6e00bcda6d5f1c59601ebf787ab0f5326d7a
Inna Vays <129948497+InnaVays@users.noreply.github.com>
2021-02-16 15:10:03 +0100

    Create database_handler

---
+++ database/database_handler.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+DB_PATH = "database/user_events.db"
+
+def setup_database():
+    """Creates the database and the necessary tables if they don't exist."""
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+
+    # Create table for storing user events
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS user_events (
+            user_id TEXT,
+            session_id TEXT,
+            strategy TEXT
+        )
+    """)
+
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    setup_database()
+    print("Database initialized successfully.")
