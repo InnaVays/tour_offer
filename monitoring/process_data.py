dd1c13390a9f6fa84b674ba2235d4b70c7f07335
Inna Vays <129948497+InnaVays@users.noreply.github.com>
2021-02-17 00:34:15 +0100

    Update process_data

--- monitoring/process_data.py
+++ monitoring/process_data.py
@@ -1,10 +1,9 @@
 import sqlite3
 import pandas as pd
-import numpy as np
 from datetime import datetime
 
 DB_PATH = "database/user_events.db"
-CSV_OUTPUT_PATH = "data/processed_results.csv"
+CSV_OUTPUT_PATH = "data/processed_experiment_results.csv"
 
 def load_data():
     conn = sqlite3.connect(DB_PATH)
@@ -17,14 +16,16 @@ def process_user_events():
     df = load_data()
 
     if df.empty:
-        print("No data available in user_events.db")
+        print("⚠️ No data available in user_events.db")
         return
 
+    # Convert timestamp to datetime
     df["timestamp"] = pd.to_datetime(df["timestamp"])
 
     # Define "high interest" events
     high_interest_events = ["email", "request_call", 'message_operator', 'book']
 
+    # Aggregate session-level data
     summary = df.groupby("session_id").agg(
         user_id=("user_id", "first"),
         strategy=("strategy", "first"),
@@ -33,12 +34,9 @@ def process_user_events():
         session_length=("timestamp", lambda x: (x.max() - x.min()).total_seconds())
     ).reset_index()
 
-def save_csv(df):
-    if not df.empty:
-        df.to_csv(CSV_OUTPUT_PATH, index=False)
-        print(f"Processed data saved to {CSV_OUTPUT_PATH}")
+    # Save to CSV
+    summary.to_csv(CSV_OUTPUT_PATH, index=False)
+
 
 if __name__ == "__main__":
-    raw_data = load_data()
-    processed_data = process_data(raw_data)
-    save_csv(processed_data)
+    process_user_events()
