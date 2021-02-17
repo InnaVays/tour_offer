24f61a2c371787d438733d3c6f26bdd8eb5b540e
Inna Vays <129948497+InnaVays@users.noreply.github.com>
2021-02-17 01:02:44 +0100

    Update process_data

--- monitoring/process_data.py
+++ monitoring/process_data.py
@@ -22,17 +22,27 @@ def process_user_events():
     # Convert timestamp to datetime
     df["timestamp"] = pd.to_datetime(df["timestamp"])
 
-    # Define "high interest" events
     high_interest_events = ["email", "request_call", 'message_operator', 'book']
 
-    # Aggregate session-level data
-    summary = df.groupby("session_id").agg(
-        user_id=("user_id", "first"),
-        strategy=("strategy", "first"),
-        clicks=("event_type", lambda x: (x == "click").sum()),
-        high_interest=("event_type", lambda x: x.isin(high_interest_events).sum()),
-        session_length=("timestamp", lambda x: (x.max() - x.min()).total_seconds())
-    ).reset_index()
+    grouped = df.groupby("session_id")
+
+    user_id = grouped["user_id"].first()
+    strategy = grouped["strategy"].first()
+
+    clicks = grouped["event_type"].apply(lambda x: (x == "click").sum())
+
+    high_interest = grouped["event_type"].apply(lambda x: x.isin(high_interest_events).sum())
+
+    session_length = grouped["timestamp"].agg(lambda x: (x.max() - x.min()).total_seconds())
+
+    summary = pd.DataFrame({
+        "session_id": grouped.size().index,  
+        "user_id": user_id,
+        "strategy": strategy,
+        "clicks": clicks,
+        "high_interest": high_interest,
+        "session_length": session_length
+    })
 
     # Save to CSV
     summary.to_csv(CSV_OUTPUT_PATH, index=False)
