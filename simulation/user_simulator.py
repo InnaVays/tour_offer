e2cd66e345489589bcc362c8669defcff3cbd9d6
Inna Vays <129948497+InnaVays@users.noreply.github.com>
2021-03-02 13:20:04 +0100

    Update simulation

--- simulation/user_simulator.py
+++ simulation/user_simulator.py
@@ -32,7 +32,7 @@ def simulate_user_activity():
     interest = max(0, int(np.random.normal(*METRIC_DISTRIBUTION[strategy]["interest"])))  
     session_length = max(1, int(np.random.normal(*METRIC_DISTRIBUTION[strategy]["session_length"])))  
 
-    start_time = time.time()
+    #start_time = time.time()
     formatted_time = time.strftime("%Y-%m-%d %H:%M:%S")
 
     # Start Session Event
