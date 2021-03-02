f680215e08f1cd982bc8eacaf9400c801e9c5c28
Inna Vays <129948497+InnaVays@users.noreply.github.com>
2021-03-02 23:03:13 +0100

    Update simulation

---
+++ run.py
@@ -0,0 +1,29 @@
+import argparse
+import config
+
+def set_mode(mode):
+    """Dynamically set mode in config.py"""
+    with open("config.py", "r") as file:
+        lines = file.readlines()
+
+    with open("config.py", "w") as file:
+        for line in lines:
+            if line.startswith("MODE = "):
+                file.write(f"MODE = \"{mode}\"\n")
+            else:
+                file.write(line)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run Travel Offer Experiment")
+    parser.add_argument("--simulation", action="store_true", help="Run in simulation mode")
+    parser.add_argument("--production", action="store_true", help="Run in production mode")
+    args = parser.parse_args()
+
+    if args.simulation:
+        set_mode("SIMULATION")
+        print("Running in SIMULATION mode...")
+    elif args.production:
+        set_mode("PRODUCTION")
+        print("Running in PRODUCTION mode...")
+    else:
+        print("⚠️ Please specify --simulation or --production")
