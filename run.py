b3375c01768c84bc2f2ed91210863b424c0068e5
Inna Vays <129948497+InnaVays@users.noreply.github.com>
2021-03-03 14:35:56 +0100

    Update run

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import argparse
-import config
+import os
 
 def set_mode(mode):
-    """Dynamically set mode in config.py"""
+    """Dynamically update config.py to set the experiment mode."""
     with open("config.py", "r") as file:
         lines = file.readlines()
 
@@ -14,12 +14,18 @@ def set_mode(mode):
                 file.write(line)
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run Travel Offer Experiment")
+    parser.add_argument("--simulation", action="store_true", help="Run in simulation mode")
+    parser.add_argument("--production", action="store_true", help="Run in production mode")
+    args = parser.parse_args()
 
     if args.simulation:
         set_mode("SIMULATION")
         print("Running in SIMULATION mode...")
+        os.system("python simulation/user_simulator.py")
     elif args.production:
         set_mode("PRODUCTION")
         print("Running in PRODUCTION mode...")
+        print("Ensure real data sources are connected!")
     else:
-        print("⚠️ Please specify --simulation or --production")
+        print("Please specify --simulation or --production")
